289de9f5264514d3fe98b412f33b4e961262f055
Alex Hedley <alex.hedley@opencastsoftware.com>
2023-11-13 17:57:24 +0000

    #19 WIP nuget support
    Co-authored-by: Rob Anderson <jamsidedown@users.noreply.github.com>
    Co-authored-by: Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>

---
+++ src/BlazorInteractive.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using BlazorInteractive.DependencyManagement;
+using Microsoft.Extensions.Logging;
+using OneOf.Types;
+
+namespace BlazorInteractive.ConsoleApp;
+
+class Program
+{
+    private static readonly ILogger _logger;
+    
+    async static Task Main(string[] args)
+    {
+        var nugetPackageGetter = new NuGetPackageGetter(_logger);
+        var package = await nugetPackageGetter.GetPackage("ironpython");
+        package.Switch(
+            success =>
+            {
+                Console.WriteLine("Success");
+            },
+            failure =>
+            {
+                Console.WriteLine("Failure");
+            }
+        );
+    }
+}
